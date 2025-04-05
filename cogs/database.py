44bffdb0be9ccaa904525a8f4fdc07e6d4c452c5
rico <121625544+elrudo78@users.noreply.github.com>
2025-04-06 02:08:06 +0530

    Update database.py

--- cogs/database.py
+++ cogs/database.py
@@ -3,7 +3,7 @@ import json
 import logging
 from discord.ext import commands, tasks
 
-class Database(commands.Cog):
+class DatabaseCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.leaderboard = {}
